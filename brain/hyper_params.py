f224ba714e742908425c7ee6110c476fe86617db
Alon Ben-Arieh <alonbenarieh@gmail.com>
2023-05-31 16:31:54 +0300

    remove done todos

--- brain/hyper_params.py
+++ brain/hyper_params.py
@@ -7,7 +7,7 @@ MEMORY_SIZE = 10 ** 5                   # The size (number of transitions) of th
 BATCH_SIZE = 64                         # The number of transitions sampled from the replay buffer
 
 GAMMA = 0.95                            # The discount factor as mentioned in the previous section
-EXPLORE_CHANCE = 0.1 if LEARN else 0    # Exploration probability  # TODO analyse performance with exploration = 0
+EXPLORE_CHANCE = 0.1 if LEARN else 0    # Exploration probability
 TAU = 0.005                             # The update rate of the target network
 LR = 1e-4                               # The learning rate of the optimizer
 
@@ -15,5 +15,3 @@ REWARD_DOWNSCALE = 1                    # Down-scaling the reward by this factor
 
 IS_SOFT = False                         # Hard or Soft update
 HARD_UPDATE_N = 10                      # Hard update every N episodes
-
-#TODO tweak hpams with Ayal. It is strange that every episode we do 6400 updates, but only make 400 new steps.
