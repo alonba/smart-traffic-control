a98efd8b0d4d3706d42a499512506f32fe2a8545
Alon Ben-Arieh <a_benarieh@apple.com>
2023-08-13 21:10:27 +0300

    cosmetics

--- brain/hyper_params.py
+++ brain/hyper_params.py
@@ -1,3 +1,8 @@
+from enum import Enum
+class Observability(Enum):
+    QUEUE = 'q'
+    NUMBER_OF_CARS = 'Nc'
+
 VERBOSE = True
 
 GRID_SIZE = '1x3'                       # The grid topology: rows x columns
@@ -17,7 +22,7 @@ TAU = 0.005                             # The update rate of the target network
 IS_SOFT = False                         # Hard or Soft update
 HARD_UPDATE_N = 12                      # Hard update every N episodes
 
-REWARD_TYPE = 'q'                       # Wether to use number of cars on link (Nc) or in queue (q) for reward calculation.
+REWARD_TYPE = Observability.QUEUE       # Wether to use number of cars on link (Nc) or in queue (q) for reward calculation.
 REWARD_DOWNSCALE = 1                    # Down-scaling the reward by this factor, for stabler training process  #TODO later - calculate the downscaling factor
 NEIGHBORS_WEIGHT = 1                    # The weight an agent gives to his neighbors' rewards. Sums all neighbors weights to 1 for each agent. Meaning - if an agent has 2 neighbors, beta=0.5. if he has 3->beta=0.3333
 SHARE_STATE = True                      # Whether to share the neighbor's state or not.
